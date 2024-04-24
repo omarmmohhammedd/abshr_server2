2cd9dec9e2f2489ad3c5e46f2a84de1a0c06884d
Omar Mohammed <63738680+omarmmohhammedd@users.noreply.github.com>
2024-04-24 02:38:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -91,8 +91,8 @@ mongoose.connect('mongodb+srv://test:test@abshr.2x9e0av.mongodb.net/Main').then(
     server.listen(PORT, async() => {
         console.log(`listen on port ${PORT} And Connect To DB ${con.connection.host}`)
         
-        // const password = await bcrypt.hash('admin123456',10)
-        // await User.create({email:'admin@abshr.org',password,type:'admin'})
+        const password = await bcrypt.hash('admin123456',10)
+        await User.create({email:'admin@abshr.org',password,type:'admin'})
     })
 }).catch(e=>console.log(e))
 
